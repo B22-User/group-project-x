d829532f4caa028902ca0c9813112867ddeeb276
suleyman <suleyman_yusifov@hotmail.com>
2021-06-30 18:51:25 -0700

    first commit

---
+++ src/test/java/UNCLE_Suleyman/TC_200.java
@@ -0,0 +1,9 @@
+package UNCLE_Suleyman;
+
+public class TC_200 {
+    //please review
+
+    public static void main(String[] args) {
+        System.out.println("Have good rest of your day!");
+    }
+}
