3094db7bf9a66b00d7a430fd61efac867dd1c24e
Ahmad Seyar Sabawoon <seyarkhan143@gmail.com>
2021-07-07 16:56:17 -0700

    First Time sharing file

---
+++ src/test/java/Khan_Seyar.java
@@ -0,0 +1,5 @@
+public class Khan_Seyar {
+    public static void main(String[] args) {
+        System.out.println("Hey everyone this is very important to practice together and make it very easy");
+    }
+}
